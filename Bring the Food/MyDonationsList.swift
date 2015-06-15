21e864fa7c3adc533926e3146a367d52f7d9078e
federicobadini <Sup3r4ed91>
2015-06-15 16:53:29 +0200

    on my way regarding tab-bar

--- Bring the Food/MyDonationsList.swift
+++ Bring the Food/MyDonationsList.swift
@@ -38,13 +38,29 @@ public class MyDonationsList: NSObject, UITableViewDataSource, UITableViewDelega
         let mainLabel = cell.viewWithTag(1000) as! UILabel
         mainLabel.text = myAvailableDonationsList[row].getDescription()
         let addressLabel = cell.viewWithTag(1001) as! UILabel
+        addressLabel.numberOfLines = 2
+        let iOS8 = floor(NSFoundationVersionNumber) > floor(NSFoundationVersionNumber_iOS_7_1)
+        if (iOS8) {
+            // do nothing, it will use automatic via the storyboard
+        } else {
+            let screenWidth = UIScreen.mainScreen().bounds.width
+            addressLabel.preferredMaxLayoutWidth = screenWidth - 89;
+        }
         addressLabel.text = myAvailableDonationsList[row].getSupplier().getAddress().getLabel()
         let expirationLabel = cell.viewWithTag(1002) as! UILabel
         expirationLabel.text = String(myAvailableDonationsList[row].getRemainingDays()) + "d"
-        if(myAvailableDonationsList[row].getRemainingDays() > 10){
-            let alarm = cell.viewWithTag(1003) as! UIImageView
-            alarm.hidden = true
+        if(myAvailableDonationsList[row].getRemainingDays() < 10){
+            let alarmIcon = cell.viewWithTag(1003) as! UIImageView
+            alarmIcon.hidden = true
         }
+        let amountLabel = cell.viewWithTag(1004) as! UILabel
+        amountLabel.text = "\(myAvailableDonationsList[row].getParcelSize())"
+        let parcelUnit = myAvailableDonationsList[row].getParcelUnit()
+        if(parcelUnit != ParcelUnit.KILOGRAMS){
+            let kgIcon = cell.viewWithTag(1005) as! UIImageView
+            kgIcon.hidden = true
+        }
+
         
         return cell
     }
