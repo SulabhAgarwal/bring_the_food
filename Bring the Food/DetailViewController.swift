80aaf6d229c8ff14d285c89e4c2b28fd650df28b
federicobadini <Sup3r4ed91>
2015-06-19 15:16:50 +0200

    map in progress

--- Bring the Food/DetailViewController.swift
+++ Bring the Food/DetailViewController.swift
@@ -9,16 +9,30 @@
 import UIKit
 import MapKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var infoPanelView: UIView!
     
     var donation: StoredDonation?
-    let regionRadius: CLLocationDistance = 1000
+    let regionRadius: CLLocationDistance = 250
+    var UIMainColor = UIColor(red: 0xf6/255, green: 0xae/255, blue: 0x39/255, alpha: 1)
+    var donationPosition: BtfAnnotation?
+
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         mainLabel.text = donation?.getDescription()
+        infoPanelView.layer.borderColor = UIMainColor.CGColor
+        infoPanelView.layer.borderWidth = 1.0
+        mapView.layer.borderColor = UIMainColor.CGColor
+        mapView.layer.borderWidth = 1.0
+        centerMapOnLocation(CLLocation(latitude: CLLocationDegrees(donation!.getSupplier().getAddress().getLatitude()!), longitude: CLLocationDegrees(donation!.getSupplier().getAddress().getLongitude()!)))
+        donationPosition = BtfAnnotation(title: donation!.getDescription(),
+            offerer: donation!.getSupplier().getName(), coordinate: CLLocationCoordinate2D(latitude: CLLocationDegrees(donation!.getSupplier().getAddress().getLatitude()!), longitude: CLLocationDegrees(donation!.getSupplier().getAddress().getLongitude()!)))
+        mapView.addAnnotation(donationPosition)
+        mapView.delegate = self
     }
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
@@ -33,6 +47,44 @@ class DetailViewController: UIViewController {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
             regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: false)
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        if let annotation = annotation as? BtfAnnotation {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+                as? MKPinAnnotationView {
+                    dequeuedView.annotation = annotation
+                    view = dequeuedView
+            } else {
+                
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -8, y: 0)
+                view.rightCalloutAccessoryView = UIButton.buttonWithType(UIButtonType.DetailDisclosure) as! UIView
+            }
+            return view
+        }
+        return nil
+    }
+}
+
+
+class BtfAnnotation: NSObject, MKAnnotation {
+    let title: String
+    let offerer: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, offerer: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.offerer = offerer
+        self.coordinate = coordinate
+        super.init()
+    }
+    
+    var subtitle: String {
+        return offerer
     }
 }
 
